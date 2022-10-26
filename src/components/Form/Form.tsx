142f04f8ed8a1c22391528d0ef73b9388dd2019b
Roman Troshin <roman.troshin98@gmail.com>
2022-10-26 20:58:02 +0300

    feat: add submit function in form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+
+import { useAppDispatch, useAppSelector } from 'hooks/useRedux';
+
+import { setQuery } from 'redux/slices/querySlice';
 
 import AddressIcon from 'components/Icons/AddressIcon';
 
 import styles from './Form.module.scss';
 
+interface FormFields {
+  query: HTMLInputElement;
+}
+
 function Form() {
+  const { query } = useAppSelector((state) => state.query);
+  const dispatch = useAppDispatch();
+
+  const setQueryOnSubmit = useCallback(
+    (event: React.FormEvent<HTMLFormElement & FormFields>) => {
+      event.preventDefault();
+
+      dispatch(setQuery(event.currentTarget.query.value));
+    },
+    [query]
+  );
+
   return (
-    <form className={styles.form} autoComplete="off">
+    <form className={styles.form} autoComplete="off" onSubmit={setQueryOnSubmit}>
       <input
+        type="text"
         className={styles.input}
         minLength={3}
         required
+        name="query"
         placeholder="Введите интересующий вас адрес"
       />
       <button type="submit" className={styles.formButton}>
