1dcd5f85ca26695acc53c201db51cdfbc431978d
Roman Troshin <roman.troshin98@gmail.com>
2022-10-26 13:12:44 +0300

    feat: add home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import AppLayout from 'components/AppLayout/AppLayout';
+import HomePage from 'pages/HomePage/HomePage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
-        <Route index element={<div>Home</div>} />
+        <Route index element={<HomePage />} />
         <Route path="address" element={<div>Address</div>} />
         <Route path="404" element={<div>Not Found</div>} />
         <Route path="*" element={<Navigate to="404" replace />} />
